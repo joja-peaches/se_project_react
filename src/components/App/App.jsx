aed4748f1920186808e68fe1518df37569c0cc73
unknown <joja.peaches7@gmail.com>
2025-02-24 14:40:44 -0600

    Fix checkboxes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -75,14 +75,12 @@ function App() {
             <legend className="modal__legend">Select the weather type:</legend>
             <label
               htmlFor="hot"
-              for="weather"
               className="modal__label modal__label_type_radio"
             >
               <input name="weather" id="hot" type="radio" className="modal__radio-input" /> Hot
             </label>
             <label
               htmlFor="warm"
-              for="weather"
               className="modal__label modal__label_type_radio"
             >
               <input name="weather" id="warm" type="radio" className="modal__radio-input" />{" "}
@@ -90,7 +88,6 @@ function App() {
             </label>
             <label
               htmlFor="cold"
-              for="weather"
               className="modal__label modal__label_type_radio"
             >
               <input name="weather" id="cold" type="radio" className="modal__radio-input" />{" "}
